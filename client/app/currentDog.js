1702ff21ae6793a6d79cb79d1ba7ad47861375a3
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-11 23:59:55 -0400

    dog layers array wip

--- client/app/currentDog.js
+++ client/app/currentDog.js
@@ -12,20 +12,37 @@ angular.module('cac', [])
   //////////////////////////////////////
 
   var images = {
-    '01': 'solid',
-    '02': 'other_name_here'
+    '0': 'solid',
+    '1': 'mask',
+    '2': 'socks',
+    '3': 'saddle',
+    '4': 'mottled',
+    '5': 'grizzled',
+    '6': 'freckles',
+    '7': 'tuzedo',
+    '8': 'blanket',
+    '9': 'outline'
   }
 
   //////////////////////////////////////
  /////// *****MAIN FACTORY***** ///////
 //////////////////////////////////////
-  var layers = [];
+  var layers;
+
+  var random = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   var newDog = function() {
+    layers = Array(random(0, 4)).fill({});
 
+    layers.forEach(function(value) {
+      value.z = random(1,9);
+      value.image = images[value.z];
+      value.hue = random(0,360);
+      value.brightness = random(0,100);
+      value.saturation = random(0,100);
+    })
   }
 
-  var orderLayers = function() {
-
-  }
 });
