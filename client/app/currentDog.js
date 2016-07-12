e5f676497edd9c3701f70764255c74574f4ca3f4
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-12 10:39:59 -0400

    basic generater complete

--- client/app/currentDog.js
+++ client/app/currentDog.js
@@ -2,7 +2,20 @@ angular.module('cac', [])
 
 .controller('currentCtrl', function ($scope, Current) {
   $scope.newDog = function() {
-    Current.newDog();
+    $scope.dog = Current.newDog();
+    $scope.dog.forEach(function(layer) {
+      layer.style = {
+        'filter': 'hue-rotate(' + layer.hue + 'deg) saturate(' + layer.saturation + '%) brightness(' + layer.brightness + '%)',
+        '-webkit-filter': 'hue-rotate(' + layer.hue + 'deg) saturate(' + layer.saturation + '%) brightness(' + layer.brightness + '%)',
+        'z-index': layer.z
+      };
+      layer.image = 'imgs/layers/' + layer.imageName + '.png';
+    });
+
+  }
+  $scope.containerStyle = {
+    'height': '' + Current.height + 'px',
+    'width': '' + Current.width + 'px'
   }
 })
 
@@ -19,7 +32,7 @@ angular.module('cac', [])
     '4': 'mottled',
     '5': 'grizzled',
     '6': 'freckles',
-    '7': 'tuzedo',
+    '7': 'tuxedo',
     '8': 'blanket',
     '9': 'outline'
   }
@@ -27,22 +40,59 @@ angular.module('cac', [])
   //////////////////////////////////////
  /////// *****MAIN FACTORY***** ///////
 //////////////////////////////////////
-  var layers;
+
+  var height = $window.innerHeight * 0.9;
+  var width = $window.innerWidth * 0.9;
 
   var random = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   var newDog = function() {
-    layers = Array(random(0, 4)).fill({});
+    var layers = [];
+    var ran = random(0,4);
 
-    layers.forEach(function(value) {
-      value.z = random(1,9);
-      value.image = images[value.z];
-      value.hue = random(0,360);
-      value.brightness = random(0,100);
-      value.saturation = random(0,100);
+    for (var i = 0; i < ran; i++) {
+      layers.push({});
+    }
+
+    layers.forEach(function(layer) {
+      layer.z = random(1,8);
+      layer.imageName = images[layer.z];
+      layer.hue = random(0,360);
+      if (layer.z === 7 || layer.z === 8) {
+        if (random(0,1)) {
+          layer.brightness = random(80, 100);
+        } else {
+          layer.brightness = random(0, 20);
+        }
+      } else {
+        layer.brightness = random(0,100);
+      }
+      layer.saturation = random(0,100);
     })
+
+    layers.push({
+      z: 0,
+      imageName: images[0],
+      hue: random(0,360),
+      brightness: random(0,100),
+      saturation: random(0,100)
+    });
+
+    layers.push({
+      z: 100,
+      imageName: images[9],
+      hue: 0,
+      brightness: 100,
+      saturation: 100
+    })
+
+    return layers;
+  }
+
+  return {
+    newDog, width, height
   }
 
 });
